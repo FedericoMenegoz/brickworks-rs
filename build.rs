13f87e9e46ddc5036e5aa0b82e6d2683a4a795d4
Federico Menegoz <menegoz.federico@gmail.com>
2025-03-20 11:37:35 +0100

    Fixed bindgen for LTO optimisation see https://github.com/rust-lang/rust-bindgen/discussions/2405

--- build.rs
+++ build.rs
@@ -1,17 +1,87 @@
 use std::env;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
+use std::process::Command;
+
+use bindgen::Builder;
 
 fn main() {
-    let src_path = Path::new("src/c_wrapper");
-
-    let bindings = bindgen::Builder::default()
-        .header(src_path.join("wrapper.h").as_os_str().to_str().unwrap())
+    let wrapper_path = "src/c_wrapper/wrapper.h";
+    let current_dir = env::current_dir().expect("Error unwrapping current_dir()");
+    let absolute_wrapper_path = current_dir
+        .join(wrapper_path)
+        .to_str()
+        .expect("Error joining current dir with wrapper path.")
+        .to_owned();
+    let output_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let static_fns_path = output_path
+        .join("extern.c")
+        .to_str()
+        .expect("Error joining $OUT_DIR with static fns.")
+        .to_owned();
+    // Tell bindgen to generate wrappers for static functions
+    let bindings = Builder::default()
+        .header(&absolute_wrapper_path)
+        .generate_comments(false)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .wrap_static_fns(true)
+        .wrap_static_fns_path(&static_fns_path)
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    // This is the path to the object file.
+    let obj_path = output_path.join("extern.o");
+    // This is the path to the static library file.
+    let lib_path = output_path.join("libextern.a");
+
+    // Compile the generated wrappers into an object file.
+    let clang_output = std::process::Command::new("clang")
+        .arg("-O")
+        .arg("-c")
+        .arg("-o")
+        .arg(&obj_path)
+        .arg(&static_fns_path)
+        .arg("-include")
+        .arg(wrapper_path)
+        .output()
+        .unwrap();
+
+    if !clang_output.status.success() {
+        panic!(
+            "Could not compile object file:\n{}",
+            String::from_utf8_lossy(&clang_output.stderr)
+        );
+    }
+
+    // Turn the object file into a static library
+    #[cfg(not(target_os = "windows"))]
+    let lib_output = Command::new("ar")
+        .arg("rcs")
+        .arg(lib_path)
+        .arg(obj_path)
+        .output()
+        .unwrap();
+    #[cfg(target_os = "windows")]
+    let lib_output = Command::new("LIB")
+        .arg(obj_path)
+        .arg(format!(
+            "/OUT:{}",
+            out_dir_path.join("libextern.lib").display()
+        ))
+        .output()
+        .unwrap();
+    if !lib_output.status.success() {
+        panic!(
+            "Could not emit library file:\n{}",
+            String::from_utf8_lossy(&lib_output.stderr)
+        );
+    }
+    // So that cargo can find the brickworks wrapped library for inline static
+    println!("cargo:rustc-link-search=native={}", output_path.display());
+    // Tell cargo to statically link against the `libextern` static library.
+    println!("cargo:rustc-link-lib=static=extern");
+
+    // Write the rust bindings.
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .write_to_file(output_path.join("bindings.rs"))
+        .expect("Cound not write bindings to the Rust file");
 }
