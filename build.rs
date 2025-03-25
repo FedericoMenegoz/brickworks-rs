0b781f7b11daae78f27ce308ad7b64072022160b
Federico Menegoz <menegoz.federico@gmail.com>
2025-03-25 16:25:15 +0100

    Fixed bindgen errors and cleaned up

--- build.rs
+++ build.rs
@@ -1,39 +1,39 @@
-use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
 use bindgen::Builder;
 
 fn main() {
-    let wrapper_path = "src/c_wrapper/wrapper.hpp";
-    let current_dir = env::current_dir().expect("Error unwrapping current_dir()");
-    let absolute_wrapper_path = current_dir
-        .join(wrapper_path)
+    let wrapper_path = PathBuf::from("src/c_wrapper/wrapper.h")
+        .canonicalize()
+        .expect("Cannot canonicalize wrapper path")
         .to_str()
-        .expect("Error joining current dir with wrapper path.")
+        .expect("The wrapper path is not a valid string.")
         .to_owned();
     let output_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+
+    // Path for the static and inline function
+    let static_filename = "static_inline_wrapper";
     let static_fns_path = output_path
-        .join("extern.c")
+        .join(static_filename.to_owned() + ".c")
         .to_str()
         .expect("Error joining $OUT_DIR with static fns.")
         .to_owned();
-    // Tell bindgen to generate wrappers for static functions
+
+    // This is the path to the object file
+    let obj_path = output_path.join(static_filename.to_owned() + ".o");
+    // This is the path to the static library file.
+    let lib_path = output_path.join("lib".to_owned() + static_filename + ".a");
+
+    // Build the bindings
     let bindings = Builder::default()
-        .header(&absolute_wrapper_path)
-        .clang_arg("-std=c++11")
+        .header(&wrapper_path)
         .generate_comments(false)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .wrap_static_fns(true)
-        .wrap_static_fns_path("./extern.c")
-        // .wrap_static_fns_path(&static_fns_path)
+        .wrap_static_fns_path(&static_fns_path)
         .generate()
         .expect("Unable to generate bindings");
-
-    // This is the path to the object file
-    let obj_path = output_path.join("extern.o");
-    // This is the path to the static library file.
-    let lib_path = output_path.join("libextern.a");
 
     // Compile the generated wrappers into an object file
     let clang_output = std::process::Command::new("clang")
@@ -41,8 +41,7 @@ fn main() {
         .arg("-c")
         .arg("-o")
         .arg(&obj_path)
-        .arg("extern.c")
-        // .arg(&static_fns_path)
+        .arg(&static_fns_path)
         .arg("-include")
         .arg(wrapper_path)
         .output()
@@ -68,7 +67,9 @@ fn main() {
         .arg(obj_path)
         .arg(format!(
             "/OUT:{}",
-            out_dir_path.join("libextern.lib").display()
+            out_dir_path
+                .join("lib".to_owned() + static_filename + ".lib")
+                .display()
         ))
         .output()
         .unwrap();
@@ -80,8 +81,8 @@ fn main() {
     }
     // So that cargo can find the brickworks wrapped library for inline static
     println!("cargo:rustc-link-search=native={}", output_path.display());
-    // Tell cargo to statically link against the `libextern` static library
-    println!("cargo:rustc-link-lib=static=extern");
+    // Tell cargo to statically link against the static wrapper
+    println!("cargo:rustc-link-lib=static={}", static_filename);
 
     // Write the rust bindings
     bindings
