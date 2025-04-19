8435be38e60382dfb889405e4ad76e7574637441
Federico Menegoz <menegoz.federico@gmail.com>
2025-04-19 19:09:25 +0200

    Refactored more tests for one_pole_wrapper

--- src/c_wrapper/one_pole_wrapper.rs
+++ src/c_wrapper/one_pole_wrapper.rs
@@ -8,7 +8,6 @@ pub(crate) struct OnePoleWrapper<const N_CHANNELS: usize> {
 
 impl<const N_CHANNELS: usize> OnePoleWrapper<N_CHANNELS> {
     pub(crate) fn new() -> Self {
-
         let states: [*mut bw_one_pole_state; N_CHANNELS] = std::array::from_fn(|_| {
             let state = Box::new(bw_one_pole_state { y_z1: 0.0 });
             Box::into_raw(state)
@@ -18,7 +17,7 @@ impl<const N_CHANNELS: usize> OnePoleWrapper<N_CHANNELS> {
             let state = Box::new(bw_one_pole_state { y_z1: 0.0 });
             Box::into_raw(state)
         });
-        
+
         let mut one_pole = OnePoleWrapper {
             coeffs: bw_one_pole_coeffs {
                 fs_2pi: Default::default(),
@@ -34,7 +33,7 @@ impl<const N_CHANNELS: usize> OnePoleWrapper<N_CHANNELS> {
             states,
             states_p,
         };
-        unsafe { 
+        unsafe {
             bw_one_pole_init(&mut one_pole.coeffs);
         }
         one_pole
@@ -46,32 +45,21 @@ impl<const N_CHANNELS: usize> OnePoleWrapper<N_CHANNELS> {
         }
     }
 
-    pub(crate) fn reset(&mut self, x0: Option<f32>, y0: Option<&mut [f32]>) {
+    pub(crate) fn reset(&mut self, x0: Option<&[f32]>, mut y0: Option<&mut [f32]>) {
         unsafe {
             bw_one_pole_reset_coeffs(&mut self.coeffs);
-            if let Some(y0_value) = y0 {
-                (0..N_CHANNELS).for_each(|i| {
-                    y0_value[i] = bw_one_pole_reset_state(
-                        &mut self.coeffs,
-                    self.states[i],
-                        x0.unwrap_or(0.),
-                    )
-                });
-            } else {
-                (0..N_CHANNELS).for_each(|i| {
-                    bw_one_pole_reset_state(
-                        &mut self.coeffs,
-                        self.states[i],
-                        x0.unwrap_or(0.),
-                    );
-                });
-            }
+            (0..N_CHANNELS).for_each(|i| {
+                let x0_value = x0.map_or(0.0, |slice| slice[i]);
+                let result = bw_one_pole_reset_state(&mut self.coeffs, self.states[i], x0_value);
+                if let Some(slice) = y0.as_deref_mut() {
+                    slice[i] = result
+                }
+            });
         }
     }
 
     pub(crate) fn process(&mut self, x: &[&[f32]], y: &mut [&mut [f32]], n_samples: usize) {
         unsafe {
-            
             let mut x_ptrs: [*const f32; N_CHANNELS] = [std::ptr::null(); N_CHANNELS];
             let mut y_ptrs: [*mut f32; N_CHANNELS] = [std::ptr::null_mut(); N_CHANNELS];
             (0..N_CHANNELS).for_each(|i| {
@@ -92,49 +80,62 @@ impl<const N_CHANNELS: usize> OnePoleWrapper<N_CHANNELS> {
 
     pub(crate) fn set_cutoff(&mut self, value: f32) {
         assert!(value >= 0., "Value must be non negative, got {value}!");
-
         unsafe {
             bw_one_pole_set_cutoff(&mut self.coeffs, value);
         }
     }
 
     pub(crate) fn set_cutoff_up(&mut self, value: f32) {
+        assert!(value >= 0., "Value must be non negative, got {value}!");
         unsafe {
             bw_one_pole_set_cutoff_up(&mut self.coeffs, value);
         }
     }
 
     pub(crate) fn set_cutoff_down(&mut self, value: f32) {
+        assert!(value >= 0., "Value must be non negative, got {value}!");
         unsafe {
             bw_one_pole_set_cutoff_down(&mut self.coeffs, value);
         }
     }
 
     pub(crate) fn set_tau(&mut self, value: f32) {
+        assert!(value >= 0., "Value must be non negative, got {value}!");
         unsafe {
             bw_one_pole_set_tau(&mut self.coeffs, value);
         }
     }
 
     pub(crate) fn set_tau_up(&mut self, value: f32) {
+        assert!(value >= 0., "Value must be non negative, got {value}!");
         unsafe {
             bw_one_pole_set_tau_up(&mut self.coeffs, value);
         }
     }
 
     pub(crate) fn set_tau_down(&mut self, value: f32) {
+        assert!(value >= 0., "Value must be non negative, got {value}.");
         unsafe {
             bw_one_pole_set_tau_down(&mut self.coeffs, value);
         }
     }
 
     pub(crate) fn set_sticky_thresh(&mut self, value: f32) {
+        assert!(
+            value >= 0. && value <= 1.0e18,
+            "Value must be in range [0.0, 1.0e18], got {value}."
+        );
         unsafe {
             bw_one_pole_set_sticky_thresh(&mut self.coeffs, value);
         }
     }
 
     pub(crate) fn set_sticky_mode(&mut self, value: bw_one_pole_sticky_mode) {
+        assert!(
+            value == bw_one_pole_sticky_mode_bw_one_pole_sticky_mode_abs
+                || value == bw_one_pole_sticky_mode_bw_one_pole_sticky_mode_rel,
+            "Sticky mode can be {bw_one_pole_sticky_mode_bw_one_pole_sticky_mode_abs} or {bw_one_pole_sticky_mode_bw_one_pole_sticky_mode_rel}, got {value}"
+        );
         unsafe {
             bw_one_pole_set_sticky_mode(&mut self.coeffs, value as u32);
         }
@@ -155,11 +156,14 @@ impl<const N_CHANNELS: usize> OnePoleWrapper<N_CHANNELS> {
 
 #[cfg(test)]
 mod tests {
+    use core::f32;
     use std::f32::consts::PI;
 
     use super::*;
     const N: usize = 2;
-    const INVERSE_2_PI: f32 = 1.0 / (2.0 * PI); 
+    const INVERSE_2_PI: f32 = 1.0 / (2.0 * PI);
+    const BW_RCPF_ERROR: f32 = 0.0013;
+    const SAMPLE_RATE: f32 = 48_000.0;
 
     #[test]
     fn new() {
@@ -168,15 +172,18 @@ mod tests {
         assert_eq!(one_pole.coeffs.cutoff_up, f32::INFINITY);
         assert_eq!(one_pole.coeffs.cutoff_down, f32::INFINITY);
         assert_eq!(one_pole.coeffs.sticky_thresh, 0.);
-        assert_eq!(one_pole.coeffs.sticky_mode, bw_one_pole_sticky_mode_bw_one_pole_sticky_mode_abs);
-        assert_eq!(one_pole.coeffs.param_changed, -1); // from brickworks lib coeffs->param_changed = ~0; // useless, just to make compilers happy about uninitialized variables
+        assert_eq!(
+            one_pole.coeffs.sticky_mode,
+            bw_one_pole_sticky_mode_bw_one_pole_sticky_mode_abs
+        );
+        assert_eq!(one_pole.coeffs.param_changed, !0); // from brickworks lib coeffs->param_changed = ~0; // useless, just to make compilers happy about uninitialized variables
     }
 
     #[test]
     fn set_sample_rate() {
-        const SAMPLE_RATE:f32 = 48_000.0;
+        const SAMPLE_RATE: f32 = 48_000.0;
         let mut f = OnePoleWrapper::<N>::new();
-        
+
         f.set_sample_rate(SAMPLE_RATE);
 
         assert_eq!(f.coeffs.fs_2pi, INVERSE_2_PI * SAMPLE_RATE);
@@ -184,30 +191,33 @@ mod tests {
 
     #[test]
     fn set_cutoff() {
-        const CUT_OFF:f32 = 1000.0;
+        const CUT_OFF: f32 = 1000.0;
         let mut f = OnePoleWrapper::<N>::new();
-        
+        f.coeffs.param_changed = 0;
+
         f.set_cutoff(CUT_OFF);
 
         assert_eq!(f.coeffs.cutoff_up, CUT_OFF);
         assert_eq!(f.coeffs.cutoff_down, CUT_OFF);
+        assert!(f.coeffs.param_changed as u32 & BW_ONE_POLE_PARAM_CUTOFF_UP != 0);
+        assert!(f.coeffs.param_changed as u32 & BW_ONE_POLE_PARAM_CUTOFF_DOWN != 0);
     }
 
     #[test]
     #[should_panic]
-    fn set_negative_cutoff_should_panic() {
-        const CUT_OFF:f32 = -1000.0;
+    fn set_cutoff_negative() {
+        const CUT_OFF: f32 = -1000.0;
         let mut f = OnePoleWrapper::<N>::new();
-        
+
         f.set_cutoff(CUT_OFF);
     }
 
     #[test]
     fn set_cutoff_up() {
-        const CUT_OFF:f32 = 1200.0;
+        const CUT_OFF: f32 = 1200.0;
         let mut f = OnePoleWrapper::<N>::new();
         f.coeffs.param_changed = 0;
-        
+
         f.set_cutoff_up(CUT_OFF);
 
         assert_eq!(f.coeffs.cutoff_up, CUT_OFF);
@@ -216,10 +226,10 @@ mod tests {
 
     #[test]
     fn set_cutoff_down() {
-        const CUT_OFF:f32 = 1200.0;
+        const CUT_OFF: f32 = 1200.0;
         let mut f = OnePoleWrapper::<N>::new();
         f.coeffs.param_changed = 0;
-        
+
         f.set_cutoff_down(CUT_OFF);
 
         assert_eq!(f.coeffs.cutoff_down, CUT_OFF);
@@ -228,57 +238,169 @@ mod tests {
 
     #[test]
     fn set_tau() {
+        let cutoff = 150.0;
+        let tau = INVERSE_2_PI / cutoff;
         let mut f = OnePoleWrapper::<N>::new();
-        f.set_tau(0.1);
+        f.coeffs.param_changed = 0;
+
+        f.set_tau(tau);
+
+        let relative_error = (f.coeffs.cutoff_down - cutoff).abs() * 100.0 / cutoff;
+        assert!(relative_error < BW_RCPF_ERROR); // see bw_rcpf in bw_math.h
+        assert_eq!(f.coeffs.cutoff_down, f.coeffs.cutoff_up);
+        assert!(f.coeffs.param_changed as u32 & BW_ONE_POLE_PARAM_CUTOFF_DOWN != 0);
+        assert!(f.coeffs.param_changed as u32 & BW_ONE_POLE_PARAM_CUTOFF_UP != 0);
     }
 
     #[test]
     fn set_tau_up() {
+        let cutoff = 1500.0;
+        let tau = INVERSE_2_PI / cutoff;
         let mut f = OnePoleWrapper::<N>::new();
-        f.set_tau_up(0.15);
+        f.coeffs.param_changed = 0;
+
+        f.set_tau_up(tau);
+
+        let relative_error = (f.coeffs.cutoff_up - cutoff).abs() * 100.0 / cutoff;
+        assert!(relative_error < BW_RCPF_ERROR);
+        assert!(f.coeffs.param_changed as u32 & BW_ONE_POLE_PARAM_CUTOFF_UP != 0);
     }
 
     #[test]
     fn set_tau_down() {
+        let cutoff = 10_000.0;
+        let tau = INVERSE_2_PI / cutoff;
         let mut f = OnePoleWrapper::<N>::new();
-        f.set_tau_down(0.05);
+        f.coeffs.param_changed = 0;
+
+        f.set_tau_down(tau);
+
+        let relative_error = (f.coeffs.cutoff_down - cutoff).abs() * 100.0 / cutoff;
+        assert!(relative_error < BW_RCPF_ERROR);
+        assert!(f.coeffs.param_changed as u32 & BW_ONE_POLE_PARAM_CUTOFF_DOWN != 0);
+    }
+
+    #[test]
+    fn set_tau_small_should_not_change_cutoff() {
+        let tau = 0.1e-9;
+        let mut f = OnePoleWrapper::<N>::new();
+        f.coeffs.param_changed = 0;
+
+        f.set_tau(tau);
+
+        assert_eq!(f.coeffs.cutoff_up, f32::INFINITY);
+        assert_eq!(f.coeffs.cutoff_down, f32::INFINITY);
+        assert!(f.coeffs.param_changed as u32 & BW_ONE_POLE_PARAM_CUTOFF_DOWN == 0);
+        assert!(f.coeffs.param_changed as u32 & BW_ONE_POLE_PARAM_CUTOFF_UP == 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn set_negative_tau() {
+        let mut f = OnePoleWrapper::<N>::new();
+        f.set_tau(-1.);
     }
 
     #[test]
     fn set_sticky_thresh() {
+        let sticky_tresh = 0.01;
         let mut f = OnePoleWrapper::<N>::new();
-        f.set_sticky_thresh(0.2);
+
+        f.set_sticky_thresh(sticky_tresh);
+
+        assert!(f.coeffs.param_changed as u32 & BW_ONE_POLE_PARAM_STICKY_THRESH != 0);
+        assert_eq!(f.get_sticky_thresh(), sticky_tresh);
+
     }
 
     #[test]
-    fn set_sticky_mode() {
+    #[should_panic]
+    fn set_sticky_tresh_negative() {
         let mut f = OnePoleWrapper::<N>::new();
-        f.set_sticky_mode(0);
+        f.set_sticky_thresh(-1.);
     }
 
     #[test]
-    fn get_sticky_thresh() {
-        let f = OnePoleWrapper::<N>::new();
-        let _ = f.get_sticky_thresh();
+    #[should_panic]
+    fn set_sticky_tresh_too_high() {
+        let mut f = OnePoleWrapper::<N>::new();
+        f.set_sticky_thresh(1.1e18);
     }
 
     #[test]
-    fn get_sticky_mode() {
-        let f = OnePoleWrapper::<N>::new();
-        let _ = f.get_sticky_mode();
+    fn set_sticky_mode_abs() {
+        let mode = bw_one_pole_sticky_mode_bw_one_pole_sticky_mode_abs;
+        let mut f = OnePoleWrapper::<N>::new();
+
+        f.set_sticky_mode(mode);
+
+        assert_eq!(f.get_sticky_mode(), mode);
+    }
+
+    #[test]
+    fn set_sticky_mode_rel() {
+        let mode = bw_one_pole_sticky_mode_bw_one_pole_sticky_mode_rel;
+        let mut f = OnePoleWrapper::<N>::new();
+
+        f.set_sticky_mode(mode);
+
+        assert_eq!(f.get_sticky_mode(), mode);
+    }
+
+    #[test]
+    #[should_panic]
+    fn set_sticky_mode_not_valid() {
+        let mode = 3;
+        let mut f = OnePoleWrapper::<N>::new();
+
+        f.set_sticky_mode(mode);
     }
 
     #[test]
     fn reset_none() {
+        let cutoff = 1200.0;
+        let sticky_tresh = 0.1;
         let mut f = OnePoleWrapper::<N>::new();
-        f.reset(Some(0.0), None);
+        f.set_cutoff(cutoff);
+        f.set_sticky_thresh(sticky_tresh);
+        f.set_sample_rate(SAMPLE_RATE);
+        let inverse= unsafe {
+             f.coeffs.fs_2pi * bw_rcpf(f.coeffs.fs_2pi + cutoff)
+        };
+
+        f.reset(None, None);
+
+        assert!(f.coeffs.param_changed == 0);
+        assert_eq!(f.coeffs.mA1u, inverse);
+        assert_eq!(f.coeffs.mA1d, inverse);
+        assert_eq!(f.coeffs.st2, sticky_tresh * sticky_tresh);
     }
+
+    // #[test]
+    // fn reset_with_input_and_output() {
+    //     let cutoff = 1000.0;
+    //     let sticky_thresh = 0.2;
+    //     let x0_input = [0.5; N];
+    //     let mut y0_output = [0.0; N];
+    //     let mut f = OnePoleWrapper::<N>::new();
+    //     f.set_cutoff(cutoff);
+    //     f.set_sticky_thresh(sticky_thresh);
+    //     f.set_sample_rate(SAMPLE_RATE);
+
+    //     f.reset(Some(&x0_input), Some(&mut y0_output));
+
+    //     for i in 0..N {
+    //         assert_eq!(f.states[i].y_z1, x0_input[i]);
+    //         assert_eq!(y0_output[i], x0_input[i]);
+    //     }
+    // }
 
     #[test]
     fn reset_some() {
         let mut f = OnePoleWrapper::<N>::new();
         let mut y = vec![0.0; N];
-        f.reset(Some(0.0), Some(&mut y));
+        let x = vec![0.0; N];
+        f.reset(Some(&x), Some(&mut y));
     }
 
     #[test]
@@ -289,7 +411,7 @@ mod tests {
         let mut y = vec![vec![0.0; N_SAMPLES]; N];
         let x_refs: Vec<&[f32]> = x.iter().map(|v| v.as_slice()).collect();
         let mut y_refs: Vec<&mut [f32]> = y.iter_mut().map(|v| v.as_mut_slice()).collect();
-        
+
         f.process(&x_refs, &mut y_refs, N_SAMPLES);
     }
 
