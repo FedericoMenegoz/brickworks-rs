6f91faf53f1928b7a42326192d40c16b662f30b6
Federico Menegoz <menegoz.federico@gmail.com>
2025-09-28 12:21:35 +0200

    Added cc crate to simplify build.rs and added bench profile

--- build.rs
+++ build.rs
@@ -1,118 +1,61 @@
 use std::collections::HashSet;
 use std::path::PathBuf;
-use std::process::Command;
-
 use bindgen::Builder;
 use bindgen::callbacks::{MacroParsingBehavior, ParseCallbacks};
 
 fn main() {
     let wrapper_path = PathBuf::from("src/c_wrapper/wrapper.h")
         .canonicalize()
-        .expect("Cannot canonicalize wrapper path")
-        .to_str()
-        .expect("The wrapper path is not a valid string.")
-        .to_owned();
-    let output_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+        .expect("Cannot canonicalize wrapper path");
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
 
-    // Path for the static and inline function
+    // Paths for the static inline wrapper
     let static_filename = "static_inline_wrapper";
-    let static_fns_path = output_path
-        .join(static_filename.to_owned() + ".c")
-        .to_str()
-        .expect("Error joining $OUT_DIR with static fns.")
-        .to_owned();
+    let static_fns_path = out_dir.join(format!("{}.c", static_filename));
 
-    // This is the path to the object file
-    let obj_path = output_path.join(static_filename.to_owned() + ".o");
-    // This is the path to the static library file.
-    let lib_path = output_path.join("lib".to_owned() + static_filename + ".a");
-
-    // Build the bindings
+    // Generate Rust bindings with bindgen
     let mut builder = Builder::default()
-        .header(&wrapper_path)
+        .header(wrapper_path.to_str().unwrap())
         .generate_comments(false)
         .parse_callbacks(Box::new(IgnoreMacros::new()))
         .derive_default(true)
         .wrap_static_fns(true)
-        .wrap_static_fns_path(&static_fns_path);
+        .wrap_static_fns_path(static_fns_path.to_str().unwrap());
 
-    // Prepare clang command for compilation
-    let mut clang_cmd = std::process::Command::new("clang");
-    clang_cmd
-        .arg("-O")
-        .arg("-c")
-        .arg("-fPIC")
-        .arg("-o")
-        .arg(&obj_path)
-        .arg(&static_fns_path)
-        .arg("-include")
-        .arg(wrapper_path);
-
-    // To define DEBUG_DEEP when not in release
+    // Set debug/release macros
     if cfg!(debug_assertions) {
-        println!("cargo:warning=INFO: DEBUG_DEEP is defined.");
-        // Define BW_DEBUG_DEEP for bindgen
         builder = builder.clang_arg("-DBW_DEBUG_DEEP");
-        // Define BW_DEBUG_DEEP for clang compilation
-        clang_cmd.arg("-D").arg("BW_DEBUG_DEEP");
-    } 
-    // To define BW_NO_DEBUG when in release
-    else {
-        // Define BW_NO_DEBUG for bindgen
+    } else {
         builder = builder.clang_arg("-DBW_NO_DEBUG");
-        // Define BW_NO_DEBUG for clang compilation
-        clang_cmd.arg("-D").arg("BW_NO_DEBUG");
     }
-
 
     let bindings = builder.generate().expect("Unable to generate bindings");
-
-    let clang_output = clang_cmd.output().unwrap();
-
-    if !clang_output.status.success() {
-        panic!(
-            "Could not compile object file:\n{}",
-            String::from_utf8_lossy(&clang_output.stderr)
-        );
-    }
-
-    // Turn the object file into a static library
-    #[cfg(not(target_os = "windows"))]
-    let lib_output = Command::new("ar")
-        .arg("rcs")
-        .arg(lib_path)
-        .arg(obj_path)
-        .output()
-        .unwrap();
-    #[cfg(target_os = "windows")]
-    let lib_output = Command::new("LIB")
-        .arg(obj_path)
-        .arg(format!(
-            "/OUT:{}",
-            output_path
-                .join("lib".to_owned() + static_filename + ".lib")
-                .display()
-        ))
-        .output()
-        .unwrap();
-    if !lib_output.status.success() {
-        panic!(
-            "Could not emit library file:\n{}",
-            String::from_utf8_lossy(&lib_output.stderr)
-        );
-    }
-    // So that cargo can find the brickworks wrapped library for inline static
-    println!("cargo:rustc-link-search=native={}", output_path.display());
-    // Tell cargo to statically link against the static wrapper
-    println!("cargo:rustc-link-lib=static={}", static_filename);
-
-    // Write the rust bindings
     bindings
-        .write_to_file(output_path.join("bindings.rs"))
-        .expect("Cound not write bindings to the Rust file");
+        .write_to_file(out_dir.join("bindings.rs"))
+        .expect("Could not write bindings file");
+
+    // Compile the generated C wrapper using cc crate
+    let mut build = cc::Build::new();
+    build.file(static_fns_path)
+        .include(wrapper_path.parent().unwrap())
+        .flag_if_supported("-O3")
+        .flag_if_supported("-flto")
+        .flag_if_supported("-fPIC");
+
+    // Optional: add debug/release macro defines for C code
+    if cfg!(debug_assertions) {
+        build.define("BW_DEBUG_DEEP", None);
+    } else {
+        build.define("BW_NO_DEBUG", None);
+    }
+
+    // Compile as a static library
+    build.compile(static_filename);
+
+    // Tell Cargo where to find the library (handled automatically by cc)
 }
 
-// See https://github.com/rust-lang/rust-bindgen/issues/687#issuecomment-450750547
+// Ignore certain macros to avoid bindgen parsing errors
 const IGNORE_MACROS: [&str; 5] = [
     "FP_INFINITE",
     "FP_NAN",
